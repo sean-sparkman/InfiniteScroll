a2de2cd672e3fa42324139508a398889cc02b28b
Sean Sparkman <sean.sparkman@gmail.com>
2015-07-30 18:36:22 -0500

    initial commit

---
+++ LoadOnScroll/InfiniteViewSource.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UIKit;
+using Foundation;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LoadOnScroll
+{
+	public class InfiniteViewSource
+		: UITableViewSource
+	{
+		Random random = new Random();
+		string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		NSString cellId = new NSString ("SearchResultCell");
+		public InfiniteViewSource ()
+		{
+			Entries = new List<string> ();
+		}
+
+		public List<string> Entries { get; set; }
+
+		public override nint RowsInSection (UITableView tableview, nint section)
+		{
+			return Entries.Count;
+		}
+
+		public override UITableViewCell GetCell (UITableView tableView, Foundation.NSIndexPath indexPath)
+		{
+			var tableCell = tableView.DequeueReusableCell (cellId);
+
+			if (tableCell == null) {
+				tableCell = new UITableViewCell (
+					UITableViewCellStyle.Subtitle,
+					cellId
+				);
+				tableCell.SelectionStyle = UITableViewCellSelectionStyle.Blue;
+				if (tableCell.TextLabel != null) {
+					tableCell.TextLabel.TextColor = UIColor.White;
+				}
+				tableCell.BackgroundColor = UIColor.FromRGB (9, 34, 65);
+
+				var backgroundView = new UIView ();
+				backgroundView.BackgroundColor = UIColor.FromRGB (88, 181, 222);
+				tableCell.SelectedBackgroundView = backgroundView;
+
+				tableCell.Accessory = UITableViewCellAccessory.DisclosureIndicator;
+				tableCell.TintColor = UIColor.White;
+			}
+
+		var entry = Entries [indexPath.Row];
+			tableCell.TextLabel.Text = entry;
+
+			return tableCell;
+		}
+
+		public void GenerateEntries (int count)
+		{
+			for (int index = 0; index < count; index++) {
+				Entries.Add (new string(
+					Enumerable.Repeat(chars, 256)
+						.Select(s => s[random.Next(s.Length)])
+						.ToArray()));
+			}
+		}
+
+		public override void WillDisplay (UITableView tableView, UITableViewCell cell, NSIndexPath indexPath)
+		{
+			if (indexPath.Row == Entries.Count - 1) {
+				GenerateEntries (25);
+				tableView.ReloadData ();
+			}
+		}
+	}
+}
+
---
+++ LoadOnScroll/TableViewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UIKit;
+using Foundation;
+using System.Linq;
+
+namespace LoadOnScroll
+{
+	public class TableViewController
+		: UITableViewController
+	{
+		InfiniteViewSource _viewSource;
+		public TableViewController ()
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			TableView.Source = _viewSource = new InfiniteViewSource ();
+			TableView.BackgroundColor = UIColor.FromRGB (88, 181, 222);
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+
+			_viewSource.GenerateEntries (25);
+			TableView.ReloadData ();
+			//var index = 1;
+			//TableView.InsertRows (Enumerable.Repeat (NSIndexPath.FromRowSection (index++, 0), 25).ToArray (), UITableViewRowAnimation.Bottom);
+		}
+	}
+}
+
